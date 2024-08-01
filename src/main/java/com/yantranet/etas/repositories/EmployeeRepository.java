c4edaa28da41fec90b313f522395e41acce98517
Akhil Varma <37375278+Akhilvatsavaya@users.noreply.github.com>
2024-07-31 17:41:26 -0700

    Delete src directory

--- src/main/java/com/yantranet/etas/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yantranet.etas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yantranet.etas.models.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{}
